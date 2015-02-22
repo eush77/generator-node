9b02cb140065d89a91d839640e8c83a0613b87d5
Eugene Sharygin <eush77@gmail.com>
2015-02-22 18:59:29 +0300

    (bare) Fill more fields to please npm

--- bare/index.js
+++ bare/index.js
@@ -9,7 +9,9 @@ var path = require('path');
 module.exports = Generator.extend({
   templating: function () {
     var options = {
-      name: _.slugify(path.basename(this.destinationRoot()))
+      name: _.slugify(path.basename(this.destinationRoot())),
+      timestamp: new Date().toISOString(),
+      path: this.destinationRoot()
     };
 
     this.template('package.json', 'package.json', options);
