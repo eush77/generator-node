46add3814fb29704c50aef807049dafae0fe4e89
Eugene Sharygin <eush77@gmail.com>
2015-02-22 18:44:27 +0300

    Add node:bare generator

---
+++ bare/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var Generator = require('yeoman-generator').Base,
+    _ = Generator.prototype._;
+
+var path = require('path');
+
+
+module.exports = Generator.extend({
+  templating: function () {
+    var options = {
+      name: _.slugify(path.basename(this.destinationRoot()))
+    };
+
+    this.template('package.json', 'package.json', options);
+  }
+});
